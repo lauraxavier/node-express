98c8eef7aa5df814c1ad5c13b0d4cebab87ac387
lauraxavier <laura.ac.xavier@gmail.com>
2023-04-16 14:54:55 -0300

    db.json e recurso de atualizar dados do aluno

--- alunos.js
+++ alunos.js
@@ -16,8 +16,20 @@ const filtrarMedia = (mediaAluno) => {
   return alunos.filter((aluno) => aluno.media >= mediaAluno);
 };
 
+const salvarAlunos = (alunos)=> {
+  const data = JSON.stringify(alunos, null, 2);
+  fs.writeFile("db.json", data, (err) => {
+    if (err) {
+      console.error("Erro ao salvar os dados dos alunos:", err);
+    } else {
+      console.log("Dados dos alunos salvos com sucesso em db.json");
+    }
+  });
+};
+
 module.exports = {
   alunos,
   filtrarNome,
   filtrarMedia,
+  salvarAlunos,
 };
--- index.js
+++ index.js
@@ -1,47 +1,92 @@
-const { alunos, filtrarNome, filtrarMedia } = require("./alunos");
 const express = require("express");
 const app = express();
+const fs = require("fs");
+const { alunos, filtrarPorNome, filtrarPorMedia, salvarAlunos } = require("./alunos.js");
+
+app.use(express.json());
 
 app.get("/alunos", (req, res) => {
-  const nome = req.query.nome;
-  const media = parseFloat(req.query.media);
-
-  let alunosFiltrados = alunos;
+  const { nome, media } = req.query;
 
   if (nome) {
-    alunosFiltrados = filtrarNome(nome);
+    const alunosFiltrados = filtrarPorNome(nome);
+    if (alunosFiltrados.length === 0) {
+      res
+        .status(404)
+        .json({ message: "Nenhum aluno encontrado com o nome informado." });
+    } else {
+      res.json(alunosFiltrados);
+    }
+  } else if (media) {
+    res.json(filtrarPorMedia(parseFloat(media)));
+  } else {
+    res.json(alunos);
   }
-
-  if (!isNaN(media)) {
-    alunosFiltrados = filtrarMedia(media);
-  }
-
-  res.json(alunosFiltrados);
 });
 
+app.post("/alunos/novo", (req, res) => {
+  const { nome, matricula, media } = req.body;
 
+  if (
+    typeof nome !== "string" ||
+    typeof matricula !== "string" ||
+    typeof media !== "number"
+  ) {
+    return res.status(400).json({ error: "Campos inválidos" });
+  }
 
-app.post("/alunos/novo", (req, res)=>{
-    const nome = req.query.nome;
-    const matricula = req.query.matricula;
-    const media = parseFloat(req.query.media);
-    
-    if (!nome || !matricula || !media){
-        return res.status(400).send("Nome, matrícula e média são campos obrigatórios!")
-    }
+  const matriculaExistente = alunos.find(
+    (aluno) => aluno.matricula === matricula
+  );
+  if (matriculaExistente) {
+    return res.status(400).json({ error: "Matrícula já cadastrada" });
+  }
 
-    if(typeof media !== "number" || isNaN(media)){
-        return res.status(400).send("A média deve ser um número válido!")
-    }
+  const novoAluno = { nome, matricula, media };
+  alunos.push(novoAluno);
 
-    const novoAluno = {nome, matricula, media};
-    alunos.push(novoAluno);
+  salvarAlunos(alunos);
+  return res.status(201).json(novoAluno);
+});
 
-    res.send("Aluno adicionado com sucesso!")
-    
-})
+app.post("/alunos/deletar/:index", (req, res) => {
+  const index = parseInt(req.params.index);
 
+  if (isNaN(index) || index < 0 || index >= alunos.length) {
+    return res.status(404).json({ error: "Aluno não encontrado" });
+  }
 
-app.listen(3000, () => {
-  console.log("http://localhost:3000/alunos");
+  const alunoRemovido = alunos.splice(index, 1)[0];
+
+  salvarAlunos(alunos);
+  return res.status(200).json({
+    message: "Aluno removido com sucesso",
+    aluno: alunoRemovido,
+  });
+});
+
+app.post("/alunos/atualizar/:index", (req, res) => {
+  const index = parseInt(req.params.index);
+  const { nome, media } = req.body;
+
+  if (isNaN(index) || index < 0 || index >= alunos.length) {
+    return res.status(404).json({ error: "Aluno não encontrado" });
+  }
+
+  if (nome) {
+    alunos[index].nome = nome;
+  }
+
+  if (media) {
+    alunos[index].media = media;
+  }
+
+  alunos[index].matricula = (index + 1).toString().padStart(3, "0");
+
+  salvarAlunos(alunos);
+  return res.status(200).json(alunos[index]);
+});
+
+app.listen(3030, () => {
+  console.log("Servidor rodando na porta http://localhost:3030/");
 });
